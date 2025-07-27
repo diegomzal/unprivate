b66b41aac4d11898af905a9badc4c92344a85c01
DiegoM-WTW <Diego.Munoz@wtwco.com>
2025-07-26 21:55:17 -0500

    Add vite to env variable

--- frontend/src/api/noteApi.ts
+++ frontend/src/api/noteApi.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
-const API_BASE_URL = import.meta.env.API_BASE_URL || 'http://localhost:3000';
+const VITE_API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000';
 
 const api = axios.create({
-    baseURL: API_BASE_URL
+    baseURL: VITE_API_BASE_URL
 });
 
 export const getNote = async (key: string) => {
